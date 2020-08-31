4ed3348cefdc5cb1b2a15db80852909ac93849eb
Grazielle Alves <graziellemontenegro@hotmail.com>
2020-08-30 23:03:10 -0300

    Diminuindo js para valor hora

--- src/main.js
+++ src/main.js
@@ -10,30 +10,12 @@ button.onclick = function () {
     console.log(valorHora);
 
     if (!isNaN(valorHora)){
-        let labelTitle;
-        let labelValor;
-
-        let HoraLabel = document.querySelector('#valor-hora');
-        if (HoraLabel.hasChildNodes()){
-            let labelTitle = document.querySelector('#valor-hora h4');
-            let labelValor = document.querySelector("#valor-hora #valor");
-            HoraLabel.removeChild(labelTitle);
-            HoraLabel.removeChild(labelValor);
-        }
         
-        labelTitle = document.createElement('h4');
-        text = document.createTextNode('Valor por hora');
-        labelTitle.appendChild(text);
-    
-        let containerValor = document.createElement('div');
-        containerValor.setAttribute('id', 'valor');
-        labelValor = document.createElement('p');
-        text = document.createTextNode(valorHora.toFixed(2).replace(".",","));
-        labelValor.appendChild(text);
-        containerValor.appendChild(labelValor);
-    
-        HoraLabel.appendChild(labelTitle);
-        HoraLabel.appendChild(containerValor);
-            
-    }
+        let labelTitle = document.querySelector("#valor-hora");
+        let labelValor = document.querySelector("#valor");
+        labelValor.innerHTML = "<p> R$ " + valorHora.toFixed(2).replace(".",",") + "</p>";
+        labelTitle.style.display = "inline";
+
+        }
+         
 }
